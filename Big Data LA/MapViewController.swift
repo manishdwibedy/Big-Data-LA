8b120cf6108e4d031381344cc55b1444f17f8b13
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-24 11:53:42 -0700

    Adding the map view and centering it to a location.

--- Big Data LA/MapViewController.swift
+++ Big Data LA/MapViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Setting the location
+        let location = CLLocationCoordinate2D(
+            latitude: 34.002395,
+            longitude: -118.386921
+        )
+        
+        // Setting the region
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        // Assigning the region to the mapView
+        mapView.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
