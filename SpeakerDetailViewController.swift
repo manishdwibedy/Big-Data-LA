10c77f8735fa52d443e84f4ead1a40349d3c8cbc
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-10 19:56:41 -0700

    Now, showing the speaker name in the title.

--- SpeakerDetailViewController.swift
+++ SpeakerDetailViewController.swift
@@ -14,6 +14,7 @@ class SpeakerDetailViewController: UIViewController {
     @IBOutlet weak var speakerTitle: UINavigationItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        speakerTitle.title = speaker
         
     }
     
