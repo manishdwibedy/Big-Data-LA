83c8da1f38665e7b776c572bcf0b654aabc56950
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-10 18:36:24 -0700

    Adding the speaker detail view controller.

---
+++ SpeakerDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SpeakerDetailViewController.swift
+//  Big Data LA
+//
+//  Created by Manish Dwibedy on 6/10/16.
+//  Copyright © 2016 Manish Dwibedy. All rights reserved.
+//
+
+import UIKit
+
+class SpeakerDetailViewController: UIViewController {
+
+}
