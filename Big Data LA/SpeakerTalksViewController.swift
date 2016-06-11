99d42c4b9d95eb19aefc6962bbfa700ebbeaec82
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-11 12:43:16 -0700

    Adding a view controller to manage the talks view.

---
+++ Big Data LA/SpeakerTalksViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SpeakerTalksViewController.swift
+//  Big Data LA
+//
+//  Created by Manish Dwibedy on 6/11/16.
+//  Copyright © 2016 Manish Dwibedy. All rights reserved.
+//
+
+import UIKit
+
+class SpeakerTalksViewController: UIViewController {
+
+}
