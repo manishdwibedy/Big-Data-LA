e2597ddff4e9d097f23bda98933ba0e4ef7b2b12
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-15 19:41:13 -0700

    Now, also showing the current location.

--- Big Data LA/MapViewController.swift
+++ Big Data LA/MapViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    let corelocationManager = CLLocationManager()
+    let locationManager: LocationManager = LocationManager.sharedInstance
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,8 +49,72 @@ class MapViewController: UIViewController {
         parking.title = "Parking"
         parking.subtitle = "Dummy"
         mapView.addAnnotation(parking)
+        
+        //
+        corelocationManager.delegate = self
+        let authCode = CLLocationManager.authorizationStatus()
+        
+        if authCode == CLAuthorizationStatus.NotDetermined && corelocationManager.respondsToSelector("requestAlwaysAuthorization") || corelocationManager.respondsToSelector("requestWhenInUseAuthorization"){
+            if(NSBundle.mainBundle().objectForInfoDictionaryKey("NSLocationAlwaysUsageDescription") != nil){
+                corelocationManager.requestAlwaysAuthorization()
+            }
+            else{
+                print("No description provided")
+            }
+        }
+        else{
+            getLocation()
+        }
+//        self.mapView.showsUserLocation = true
+//        
+//        self.mapView.removeAnnotations(self.mapView.annotations)
+//        
+//        
+//        
+//        
+//        
+//        locationManager.delegate = self
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.requestAlwaysAuthorization()
+//        locationManager.startUpdatingLocation()
+//        
+//        
+//        let currentLocation = self.locationManager.location
+//        
+//        let latitude: Double = currentLocation!.coordinate.latitude
+//        let longitude: Double = currentLocation!.coordinate.longitude
+//        
+//        print("current latitude :: \(latitude)")
+//        print("current longitude :: \(longitude)")
     }
 
+    func getLocation(){
+        self.mapView.showsUserLocation = true
+
+        locationManager.startUpdatingLocationWithCompletionHandler { (latitude, longitude, status, verboseMessage, error) in
+//            self.displayLocation(CLLocation(latitude: latitude, longitude: longitude))
+        }
+        
+    }
+    
+    func displayLocation(location:CLLocation){
+        mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude), span: MKCoordinateSpanMake(0.05, 0.05)), animated: true)
+        
+        let locationPinCoord = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationPinCoord
+        
+        mapView.addAnnotation(annotation)
+        mapView.showAnnotations([annotation], animated: true)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status != CLAuthorizationStatus.NotDetermined || status != CLAuthorizationStatus.Denied || status != CLAuthorizationStatus.Restricted{
+            getLocation()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
