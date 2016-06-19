a3f6c088e65b89021f6ae87c90967eb84f5d7d7f
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-19 09:58:03 -0700

    Removing the iOS project from the android branch.

--- iOS/Pods/Target Support Files/Pods-Big Data LA/Pods-Big Data LA-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Big_Data_LA : NSObject
-@end
-@implementation PodsDummy_Pods_Big_Data_LA
-@end
--- iOS/Pods/Target Support Files/Pods-Big Data LATests/Pods-Big Data LATests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Big_Data_LATests : NSObject
-@end
-@implementation PodsDummy_Pods_Big_Data_LATests
-@end
