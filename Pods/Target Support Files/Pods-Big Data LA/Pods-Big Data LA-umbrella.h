7f78d47d20323c5c8765f0f0aac737417325b585
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-06-19 09:59:34 -0700

    Removed the ios project from the root folder as it is now already moved in the iOS folder. Also removing the Android folder.

--- Pods/Target Support Files/Pods-Big Data LA/Pods-Big Data LA-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Big_Data_LAVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Big_Data_LAVersionString[];
-
--- Pods/Target Support Files/Pods-Big Data LAUITests/Pods-Big Data LAUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Big_Data_LAUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Big_Data_LAUITests
-@end
